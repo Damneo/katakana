13db7d60cc366d36f939fe3e4630fd773e92df73
Damien Rollot <damien.rollot@rts.ch>
2016-07-29 13:46:51 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,109 @@
+var hiragana = [
+
+	{"a":"ア"},{"i":"イ"},{"u":"ウ"},{"e":"エ"},{"o":"オ"},
+	{"ka":"カ"},{"ki":"キ"},{"ku":"ク"},{"ke":"ケ"},{"ko":"コ"},
+	{"sa":"サ"},{"shi":"シ"},{"su":"ス"},{"se":"セ"},{"so":"ソ"},
+	{"ta":"タ"},{"chi":"チ"},{"tsu":"ツ"},{"te":"テ"},{"to":"ト"},
+	{"na":"ナ"},{"ni":"ニ"},{"nu":"ヌ"},{"ne":"ネ"},{"no":"ノ"},
+	{"ha":"ハ"},{"hi":"ヒ"},{"fu":"フ"},{"he":"ヘ"},{"ho":"ホ"},
+	{"ma":"マ"},{"mi":"ミ"},{"mu":"ム"},{"me":"メ"},{"mo":"モ"},
+	{"ya":"ヤ"},{"yu":"ユ"},{"yo":"ヨ"},
+	{"ra":"ラ"},{"ri":"リ"},{"ru":"ル"},{"re":"レ"},{"ro":"ロ"},
+	{"wa":"ワ"},{"wo":"ヲ"},
+	{"n":"ン"},
+];
+
+function shuffle(sourceArray) {
+
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+        var temp = sourceArray[j];
+        sourceArray[j] = sourceArray[i];
+        sourceArray[i] = temp;
+    }
+    return sourceArray;
+}
+
+//build the div item with object. show is the symbol we display : hiragana or romaji
+function generateItem(obj,show) {
+
+	var display = (show == "romaji") ? Object.keys(obj) : obj[Object.keys(obj)];
+	var hide = (show == "romaji") ? obj[Object.keys(obj)] : Object.keys(obj);
+
+	return "<div class='item' attr-hide='"+ hide +"'><div class='show'>"+ display +"</div><input type='text'></div>";
+}
+
+function generateCopyHiragana() {
+
+	$("#copy-hiragana").empty();
+	$(hiragana).each(function(k,o){
+		
+		$("#copy-hiragana").append("<input data-clipboard-text='"+o[Object.keys(o)]+"' class='hiragana-bn' type='button' value='"+o[Object.keys(o)]+"'>");
+	});
+}
+
+function generateItems(show) {
+
+	$("#container").empty();
+	var mixed_hiragana = shuffle(hiragana);
+	$(mixed_hiragana).each(function(k,v){
+		
+		$("#container").append(generateItem(v, show));
+	});
+
+	checkInput();
+}
+
+function checkInput() {
+
+	$(".item input").on("keyup", function(){
+		var hide = $(this).parent().attr("attr-hide");
+		var text = $(this).val();
+
+		$(this).parent().removeClass("error");
+		if (hide == text) {
+			$(this).parent().removeClass("error");
+			$(this).parent().addClass("success");
+			updateScore();
+		} else {
+			$(this).parent().removeClass("success");
+			updateScore();
+		}
+	});
+
+	$(".item input").on("focusout", function(){
+
+		var text = $(this).val();
+		$(this).parent().removeClass("error");
+
+		if (text != "" && $(this).parent().attr("attr-hide") != text) {
+			$(this).parent().addClass("error");
+		}
+	});
+}
+
+function updateScore() {
+
+	$(".score").empty().text("Score : " + $(".item.success").length);
+}
+
+//MAIN
+$(document).ready(function(){
+
+	$(".generate.hira").click(function() {
+		generateItems("hiragana");
+		$("#copy-hiragana").hide();
+		updateScore();
+	});
+
+	$(".generate.roma").click(function() {
+		generateCopyHiragana();
+		generateItems("romaji");
+		$("#copy-hiragana").show();
+		new Clipboard('#copy-hiragana input');
+		updateScore();
+	});
+});
+
+
